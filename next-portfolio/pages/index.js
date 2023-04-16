5452587c855786976e6940a9c3c949528ec17a39
GoDoyeon <97godo@naver.com>
2023-04-16 18:27:05 +0900

    Feat : ㅅTailwindCSS 설정, footer, header 설정

---
+++ next-portfolio/pages/index.js
@@ -0,0 +1,16 @@
+import Layout from '../components/layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      <div>
+        {/* <Head>
+          <title>포트폴리오</title>
+          <meta name='description' content='오늘도 빡코딩' />
+        </Head> */}
+
+        <h1 class='text-3xl font-bold underline'>Hello world!</h1>
+      </div>
+    </Layout>
+  );
+}
